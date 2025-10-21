94b0492be506e5b17b8a43aaa13f1d23ce45d5ea
e1b23017 <e1b23017@oit.ac.jp>
2025-10-21 14:17:47 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/kadai4/kaizi/dbsql/DbsqlApplication.java
@@ -0,0 +1,13 @@
+package oit.is.kadai4.kaizi.dbsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/kadai4/kaizi/dbsql/DbsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.kadai4.kaizi.dbsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
