488339add94355f8ef722ed89722b9bebdcb4147
meganedayo <e1b23021@oit.ac.jp>
2025-10-21 14:46:31 +0900

    認証処理を実装

---
+++ src/main/java/oit/is/kadai4/kaizi/dbsql/security/Sample3AuthConfiguration.java
@@ -0,0 +1,34 @@
+package oit.is.kadai4.kaizi.dbsql.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+
+public class Sample3AuthConfiguration {
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+
+    // ユーザ名，パスワード，ロールを指定してbuildする
+    // このときパスワードはBCryptでハッシュ化されているため，{bcrypt}とつける
+    // ハッシュ化せずに平文でパスワードを指定する場合は{noop}をつける
+    // user1/p@ss,user2/p@ss,admin/p@ss
+
+    UserDetails inu = User.withUsername("inu")
+        .password("{bcrypt}$2y$05$EsUVssiTCJL0JZvrf1v4RuVZjKg6eElJ0IvrRdd8APWLCMYbD.Iqy").roles("USER").build();
+    UserDetails neko = User.withUsername("neko")
+        .password("{bcrypt}$2y$05$8XCyj1MxtIFPOuz/7uHcNOiTKv/S11gLCGKCxjk2/L389KrHAAmha").roles("USER").build();
+    UserDetails admin = User.withUsername("admin")
+        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("ADMIN").build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(inu, neko, admin);
+  }
+}
